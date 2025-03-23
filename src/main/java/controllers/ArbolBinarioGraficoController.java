f1e1578c510058b97175ffa02d0525e2a91e0b68
favasquezUMG <fvasquezl6@miumg.edu.gt>
2025-03-23 16:54:12 -0600

    Se añadió la funcion de ingresar datos a partir de texto separado por comas

--- src/main/java/controllers/ArbolBinarioGraficoController.java
+++ src/main/java/controllers/ArbolBinarioGraficoController.java
@@ -7,29 +7,25 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class ArbolBinarioGraficoController {
     ArbolBinario arbol = new ArbolBinario();
     StringBuilder listado = new StringBuilder();
-    StringTokenizer listaDatos;
+    Stage stage;
     GraphicsContext gc;
-
-    @FXML
-    private Canvas canvas;
-
-    @FXML
-    private Label lblMensaje;
-
-    @FXML
-    private TextField txtFIngresarDato;
-
-    @FXML
-    private TextField txtRecorrido;
 
     //Dibujar el arbol
     public void initialize() {
@@ -46,6 +42,35 @@ public class ArbolBinarioGraficoController {
     private void limpiarCanvas() {
         gc.setFill(Color.WHITE);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+    //Leer Archivo
+    public  void leerArchivo(Path ruta){
+        try{
+            List<String> lineas = Files.readAllLines(ruta);
+            for(String linea : lineas){
+                txtFIngresarDato.setText(linea);
+            }
+        } catch (IOException e){
+            lblMensaje.setText("Error al leer el archivo :(");
+        }
+    }
+
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private TextField txtFIngresarDato;
+
+    @FXML
+    private TextField txtRecorrido;
+
+    @FXML
+    void Enter(KeyEvent event) {
+
     }
 
     @FXML
@@ -64,12 +89,19 @@ public class ArbolBinarioGraficoController {
 
     @FXML
     void ingresarDato(ActionEvent event) {
+        limpiarCanvas();
         txtRecorrido.setText("");
-        int dato = Integer.parseInt(txtFIngresarDato.getText());
-        arbol.insertar(dato);
+        String linea = txtFIngresarDato.getText();
+        StringTokenizer listaDatos = new StringTokenizer(txtFIngresarDato.getText(), ",");
+
+        while (listaDatos.hasMoreTokens()){
+            String tokenActual = listaDatos.nextToken();
+            listado.append(tokenActual).append(", ");
+            int dato = Integer.parseInt(tokenActual);
+            arbol.insertar(dato);
+        }
+        lblMensaje.setText(listado+"");
         txtFIngresarDato.setText("");
-        listado.append(dato).append(", ");
-        lblMensaje.setText(listado.toString());
     }
 
     @FXML
@@ -94,4 +126,14 @@ public class ArbolBinarioGraficoController {
         txtRecorrido.setText("Reiniciado");
         lblMensaje.setText("...");
     }
+
+    @FXML
+    void seleccionarArchivo(ActionEvent event) {
+        FileChooser archivo = new FileChooser();
+        archivo.setTitle("Seleccione un archivo .txt");
+
+        File archivoSeleccionado = archivo.showOpenDialog(stage);
+        leerArchivo(archivoSeleccionado.toPath());
+    }
+
 }
